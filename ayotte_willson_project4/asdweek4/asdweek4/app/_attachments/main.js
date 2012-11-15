7e9952fac969f9717026d7091a3f10be33ddeaac
Ezcaflowne <ezcaflowne@fullsail.edu>
2012-11-14 19:11:15 -0800

    wed commit

--- ayotte_willson_project4/asdweek4/asdweek4/app/_attachments/main.js
+++ ayotte_willson_project4/asdweek4/asdweek4/app/_attachments/main.js
@@ -18,11 +18,184 @@ $('#home').on('pageinit', function(){
 // });
 
 //PAGE SHOW
-$(document).on('pageshow', '#details', function(){
+//VACANT AVAILABLE
+$('#vaa').live('pageshow', function(){
+    $.couch.db('asdweek4').view('app/vaa', {
+        success: function(result){
+            //console.log(result);
+            $('#vaaList').empty();
+            $('<li>').attr('data-role', 'list-divider')
+                     .text('Apartment Number - Apartment Size').appendTo('#vaaList');
+            $.each(result.rows, function( index, apartments ){
+                console.log(apartments.value.aptType);
+                var aptType = apartments.value.aptType;
+                if (aptType[1] === "Vacant Available"){
+                    var aptNum   = apartments.value.aptNum;
+                    var aptSize  = apartments.value.aptSize;
 
-//	Display details for a specific item
+                    $('<li>').append(
+                        $('<a>')
+                        .attr('href', "details.html?aptSize=" + aptSize[1])
+                        .text(aptNum[1] + " - " + aptSize[1])
+                        ).appendTo('#vaaList');
+                }
+            });
+            $('vaaList').listview('refresh');
+        }
+    });
 	
 });
+//END VACANT AVAILABLE
+
+//VACANT RENTED
+$('#var').live('pageshow', function(){
+    $.couch.db('asdweek4').view('app/var', {
+        success: function(result){
+            //console.log(result);
+            $('#varList').empty();
+            $('<li>').attr('data-role', 'list-divider')
+                     .text('Apartment Number - Apartment Size').appendTo('#varList');
+            $.each(result.rows, function( index, apartments ){
+                var aptType = apartments.value.aptType;
+                if (aptType[1] === "Vacant Rented"){
+                    var aptNum   = apartments.value.aptNum;
+                    var aptSize  = apartments.value.aptSize;
+
+                    $('<li>').append(
+                        $('<a>')
+                        .attr('href', "details.html?aptSize=" + aptSize[1])
+                        .text(aptNum[1] + " - " + aptSize[1])
+                        ).appendTo('#varList');
+                }
+            });
+            $('varList').listview('refresh');
+        }
+    });
+    
+});
+//END VACANT RENTED
+
+//NOTICE AVAILABLE
+$('#noa').live('pageshow', function(){
+    $.couch.db('asdweek4').view('app/noa', {
+        success: function(result){
+            //console.log(result);
+            $('#noaList').empty();
+            $('<li>').attr('data-role', 'list-divider')
+                     .text('Apartment Number - Apartment Size').appendTo('#noaList');
+            $.each(result.rows, function( index, apartments ){
+                var aptType = apartments.value.aptType;
+                if (aptType[1] === "Notice Available"){
+                    var aptNum   = apartments.value.aptNum;
+                    var aptSize  = apartments.value.aptSize;
+
+                    $('<li>').append(
+                        $('<a>')
+                        .attr('href', "details.html?aptSize=" + aptSize[1])
+                        .text(aptNum[1] + " - " + aptSize[1])
+                        ).appendTo('#noaList');
+                }
+            });
+            $('noaList').listview('refresh');
+        }
+    });
+    
+});
+//END NOTICE AVAILABLE
+
+var urlVars = function(){
+    var urlData     = $($.mobile.activePage).results('url');
+    var urlParts    = urlData.split('?');
+    var urlPairs    = urlParts[1].split('&');
+    var urlValues   = {};
+    for (var pair in urlPairs) {
+        var keyValue    = urlPairs[pair].split('=');
+        var key         = decodeURIComponet(keyValue[0]);
+        var value       = decodeURIComponet(keyValue[1]);
+        urlValues[key]  = value;
+    }
+    return urlValues;
+};
+
+$('#apartments').live('pageshow', function(){
+    var key = urlVars()['aptNum'];
+    //console.log(key)
+    $('#apartmentList').empty();
+    $.couch.db('asdweek4').view('app/var', {
+        //console.log(result);
+        success: function(result){
+            $.each(result.rows, function(index, apartments){
+                var aptNum       = apartments.value.aptNum;
+                if ( aptNum[1] === key) {
+                    var aptType      = apartments.value.aptType;
+                    var aptSize      = apartments.value.aptSize;
+                    var vacDate      = apartments.value.vacDate;
+                    var rdyDate      = apartments.value.rdyDate;
+                    var isWhiteLock  = apartments.value.isWhiteLock;
+                    var isPower      = apartments.value.isPower;
+                    var condition    = apartments.value.condition;
+                    var comments     = apartments.value.comments;
+                    var id  = apartments.id;
+                    var rev = apartments.value.rev;
+                    console.log(id);
+                    console.log(rev);
+                    $('deleteApt').on('click', function(){
+                        var ask = confirm("Are you sure you would like to delete this Apartment?");
+                        if (ask){
+                            var doc = {
+                                _id: id,
+                                _rev: rev
+                            };
+                            $.couch.db('asdweek4').removeDoc(doc, {
+                                success: function(result) {
+                                    console.log(result);
+                                    $.mobile.changePage($('gold'));
+                                    alert('Apartment Deleted');
+                                },
+                                error: function(status) {
+                                    console.log(status);
+                                }
+                            });
+                        }
+                        else{
+                            alert('Apartment not deleted!');
+                        }
+                    });
+                    var apartments = {
+                        _id: id,
+                        _rev: rev,
+                        aptType: aptType,
+                        aptNum: aptNum,
+                        aptSize: aptSize,
+                        vacDate: vacDate,
+                        rdyDate: rdyDate,
+                        isWhiteLock: isWhiteLock,
+                        isPower: isPower,
+                        condition: condition,
+                        comments: comments
+                    }
+                    $('editApt').on('click', function(){
+                        editApt( apartments );
+                    });
+                    $(' ' +
+                        '<div class="apartments">' +
+                        '<p>'  + aptType[0]         + " " + aptType[1] +
+                        '<br>' + aptNum[0]          + " " + aptNum[1] + 
+                        '<br>' + aptSize[0]         + " " + aptSize[1] + 
+                        '<br>' + vacDate[0]         + " " + vacDate[1] + 
+                        '<br>' + rdyDate[0]         + " " + rdyDate[1] + 
+                        '<br>' + isWhiteLock[0]     + " " + isWhiteLock[1] + 
+                        '<br>' + isPower[0]         + " " + isPower[1] +
+                        '<br>' + condition[0]       + " " + condition[1] +
+                        '<br>' + comments[0]        + " " + comments[1] + '</p>' +
+                        '<div>'
+                        ).appendTo('#apartmentList');
+                }
+            });
+        }
+    });
+});
+
 //END PAGE SHOW
 
 //NOTICE AVAILABLE DATA
@@ -50,7 +223,7 @@ $( '#noa' ).on('pageinit', function(){
                		);  
                 $( ' ' + 
 					'<li class="apartments">' +
-					'<p>' + aptSize + 
+					'<p>'  + aptSize + 
 					'<br>' + vacDate + 
 					'<br>' + rdyDate +
 					'<br>' + isWhiteLock +
@@ -315,28 +488,52 @@ var getData = function(){
     autoFillApartments();
     }
         
-    var makeList = $('<div>');
-    $('#showData').append(makeList)
-    for (var i = 0, len = localStorage.length; i < len; i++) {
-        
-            var makeLi = $('<li>').css('background','black');
-            var linksLi = $('<li>');
-            makeList.append(makeLi);
-            var key = localStorage.key(i);
-            var value = localStorage.getItem(key);
-            var obj = JSON.parse(localStorage.getItem(key));
-            var makeSubList = $('<ul>');
-            makeLi.append(makeSubList);
-            getImage(obj.aptType[1], makeSubList);
-        for (var n in obj) {
-            var makeSubLi = $('<li>')
-            makeSubList.append(makeSubLi);
-            var optSubText = $('<p>' + obj[n][0]+" "+obj[n][1] + '</p>').appendTo(makeLi);
-            makeSubLi.html(optSubText);
-            makeSubList.append(linksLi);
+    var makeDiv = document.createElement('div');
+    makeDiv.setAttribute('id', 'items');
+    var makeList = document.createElement('ul');
+    makeDiv.appendChild(makeList);
+    $('#showData').append(makeDiv);
+    for( var i = 0, len = localStorage.length; i < len; i++){
+        var makeLi      = document.createElement('li');
+        var linksLi     = document.createElement('li');
+        makeList.appendChild(makeLi);
+        var key         = localStorage.key(i);
+        var value       = localStorage.getItem(key);
+        var obj         = JSON.parse(value);
+        var makeSubList = document.createElement('ul');
+        makeLi.appendChild(makeSubList);
+        for (var n in obj){
+            var makeSubList     = document.createElement('li');
+            makeSubList.appendChild(makeSubLi);
+            var optSubText      = obj[n][0] + " " + obj[n][1];
+            makeSubLi.innerHTML = optSubText;
+            makeSubList.appendChild(linksLi);
         }
         makeItemLinks(localStorage.key(i), linksLi);
     }
+
+    // var makeList = $('<div>');
+    // $('#showData').append(makeList)
+    // for (var i = 0, len = localStorage.length; i < len; i++) {
+        
+    //         var makeLi = $('<li>').css('background','black');
+    //         var linksLi = $('<li>');
+    //         makeList.append(makeLi);
+    //         var key = localStorage.key(i);
+    //         var value = localStorage.getItem(key);
+    //         var obj = JSON.parse(localStorage.getItem(key));
+    //         var makeSubList = $('<ul>');
+    //         makeLi.append(makeSubList);
+    //         getImage(obj.aptType[1], makeSubList);
+    //     for (var n in obj) {
+    //         var makeSubLi = $('<li>')
+    //         makeSubList.append(makeSubLi);
+    //         var optSubText = $('<p>' + obj[n][0]+" "+obj[n][1] + '</p>').appendTo(makeLi);
+    //         makeSubLi.html(optSubText);
+    //         makeSubList.append(linksLi);
+    //     }
+    //     makeItemLinks(localStorage.key(i), linksLi);
+    // }
 
 };
 
@@ -351,12 +548,11 @@ var getImage = function (catName, makeSubList) {
     };
 
 
-var editItem = function () {
+var editItem = function (apartments) {
        
-        var key = localStorage.getItem(this.key);
-        var item = JSON.parse(value);
-        console.log(value);
-        console.log(localStorage)
+        $.mobile.changePage($('#addItem'));
+        item = apartments;
+        console.log(apartments);
 
         $('#aptType').val(item.aptType[1]);
         $('#aptNum').val(item.aptNum[1]);
@@ -368,63 +564,132 @@ var editItem = function () {
         $('#condition').val(item.condition[1]);
         $('#comments').val(item.comments[1]);
 
-        thiskey = this.key
-        $('submit').on('click', storeData(thiskey));
+        $('submit').on('click', storeData(item));
 };
 
 var makeItemLinks = function (key, linksLi) {
 
     // add edit single item link
-        var editLink = $('<n>')
-                        .attr('data-role','button')
-                        .html('Edit Apartment')
-                        .css('padding-top','10px')
-                        .attr('this',key)
-                        .on('click', editItem)
-                        editLink.key = key;
-                        editLink.appendTo(linksLi);
-        ;
+        var editLink        = document.createElement('n');
+        editLink.href       = '#';
+        editLink.key        = key;
+        var editText        = 'Edit Apartment';
+        editLink.addEventListener ('click', editItem);
+        editLink.innerHTML  = editText;
+        linksLi.appendChild(editLink); 
+        
+        // $('<n>')
+        //                 .attr('data-role','button')
+        //                 .html('Edit Apartment')
+        //                 .css('padding-top','10px')
+        //                 .attr('this',key)
+        //                 .on('click', editItem)
+        //                 editLink.key = key;
+        //                 editLink.appendTo(linksLi);
+        // ;
 
 
     // Add delete single item Link
-        var deleteLink = $('<li>')
-                        .attr('data-role','button')
-                        .text('Delete Apartment')
-                        .css('padding-top','10px')
-                        .attr('key', key)
-                        .on('click', deleteItem)
-                        deleteLink.key = key;
-                        deleteLink.appendTo(linksLi);
-        ;
+        var deleteLink      = document.createElement('n');
+        deleteLink.href     = '#';
+        deleteLink.key      = key;
+        var deleteText      = 'Delete Apartment';
+        deleteLink.addEventListener('click',deleteItem);
+        deleteLink.innerHTML = deleteText;
+        linksLi.appendChild(deleteLink);
+
+        // $('<li>')
+        //                 .attr('data-role','button')
+        //                 .text('Delete Apartment')
+        //                 .css('padding-top','10px')
+        //                 .attr('key', key)
+        //                 .on('click', deleteItem)
+        //                 deleteLink.key = key;
+        //                 deleteLink.appendTo(linksLi);
+        // ;
 };      
 
 
 var storeData = function (key){
-        // If there is not key this means this is a brand new item and need a new key.
-        if(!key){
-            var id = Math.floor(Math.random()*10000001);    
-        } else {
-            // Set the id to the existing key we are editing so that it will save over the data.
-            // The key is the same key thats been passed along from the editSubmit event handler
-            // to the validate function, and then passed here, into the storeData function.
-            id = key;
+    var id;
+    if (key){
+        console.log('key:' + key);
+        $.couch.db('asdweek4').openDoc(key._id,{
+            success: function(result) {
+                console.log(result);
+            },
+            error: function(status){
+                console.log(status);
+            }
+        });
+        $('#submit').on('click', function(){
+            doc = {
+                _id: key._id,
+                _rev: key._rev,
+            }   
+            doc.aptType         = ['Apartment Type:',       $('#aptType').val()];
+            doc.aptNum          = ['Apartment Number:',     $('#aptNum').val()];
+            doc.aptSize         = ['Apartment Size:',       $('#aptSize').val()];
+            doc.vacDate         = ['Vacate Date:',          $('#vacDate').val()];
+            doc.rdyDate         = ['Ready Date:',           $('#rdyDate').val()];
+            doc.isWhiteLock     = ['Whitelock:',            $('#isWhiteLock').val()];
+            doc.isPower         = ['Power:',                $('#isPower').val()];
+            doc.condition       = ['Condition:',            $('#condition').val()];
+            doc.comments        = ['Comments:',             $('#comments').val()];
+
+        console.log(doc);
+        $.couch.db('#asdweek4').saveDoc(doc, {
+            success: function(result) {
+                console.log(result);
+                $.mobile.changePage($('#index'));
+                alert('Apartment Updated!');
+            },
+            error: function(status) {
+                console.log(status);
+            }
+        });
+        });
+                return false;
+    }
+    else {
+        var id      = Math.floor( Math.random() * 10000001);
+        var item            ={};
+
+        item.aptType        = ['Apartment Type:',       $('#aptType').val()];
+        item.aptNum         = ['Apartment Number:',     $('#aptNum').val()];
+        item.aptSize        = ['Apartment Size:',       $('#aptSize').val()];
+        item.vacDate        = ['Vacate Date:',          $('#vacDate').val()];
+        item.rdyDate        = ['Ready Date:',           $('#rdyDate').val()];
+        item.isWhiteLock    = ['Whitelock:',            $('#isWhiteLock').val()];
+        item.isPower        = ['Power:',                $('#isPower').val()];
+        item.condition      = ['Condition:',            $('#condition').val()];
+        item.comments       = ['Comments:',             $('#comments').val()];
+    //item._id          =[item.aptType[1]];
+
+    var doc = {
+                _id: item.aptType[1]
+    };
+    doc.aptType         = ['Apartment Type:',       $('#aptType').val()];
+    doc.aptNum          = ['Apartment Number:',     $('#aptNum').val()];
+    doc.aptSize         = ['Apartment Size:',       $('#aptSize').val()];
+    doc.vacDate         = ['Vacate Date:',          $('#vacDate').val()];
+    doc.rdyDate         = ['Ready Date:',           $('#rdyDate').val()];
+    doc.isWhiteLock     = ['Whitelock:',            $('#isWhiteLock').val()];
+    doc.isPower         = ['Power:',                $('#isPower').val()];
+    doc.condition       = ['Condition:',            $('#condition').val()];
+    doc.comments        = ['Comments:',             $('#comments').val()];
+
+    $.couch.db('asdweek4').saveDoc(doc, {
+        success: function(result) {
+            console.log(result);
+        },
+        error: function(status) {
+            console.log(status);
         }
-        
-        var item                = {};
-            item.aptType        = ["Apartment Type:", $("#aptType").val()];
-            item.aptNum         = ["Apartment Number:", $("#aptNum").val()];
-            item.aptSize        = ["Apartment Size:", $("#aptSize").val()];
-            item.vacDate        = ["Vacate:", $("#vacDate").val()];
-            item.rdyDate        = ["Ready:", $("#rdyDate").val()];
-            item.isWhiteLock    = ["Whitelock:", $("#isWhiteLock").val()];
-            item.isPower        = ["Power:", $("#isPower").val()];
-            item.condition      = ["Condition:", $("#condition").val()];
-            item.comments       = ["Comments:", $("#comments").val()];
-        // Save data into localStorage: Use Stringify to convert our object to a string.
-        localStorage.setItem(id, JSON.stringify(item));
-        alert("Apartment is Saved!");
-        //$('#showData').listview('refresh');
-        window.location.reload();
+    });
+    localStorage.setItem( id, JSON.stringify(item));
+    alert('Apartment Added');
+    }
 }; 
 
 console.log(localStorage);
@@ -464,6 +729,8 @@ var clearLocal = function(){
     $( '#clearLink'   ).on( 'click', clearLocal );
     $( '#addNew'      ).on( 'click', '#addItem' );//.css('display','none');  FIX THIS SO IT TAKES ME BACK TO FORMPAGE
 
+    $('#apartmentFormErrors').css('display','none')
+    
 // $('<li><a href="#">New Link</a></li>').appendTo('#nav');
 // $.mobile.changePage('#searchResult');
 
